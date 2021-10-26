7af7c0b0df3433b42bc60e14a2a64664f4e0955b
Sufiyan Adhikari <dumbpyx@gmail.com>
2021-10-26 21:58:01 +0530

    Add wacom tab bindings

---
+++ zshfunc/set_wacom_tab.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+TABLET='HUION Huion Tablet_H640P Pad pad'
+if xsetwacom --list devices | grep -q $TABLET
+then
+    xsetwacom --set $TABLET Button 1 "key +ctrl +z -z -ctrl"
+    xsetwacom --set $TABLET Button 2 "key e"
+    xsetwacom --set $TABLET Button 3 "key b"
+    xsetwacom --set $TABLET Button 8 "key +"
+    xsetwacom --set $TABLET Button 9 "key -"
+    # set last button to ctrl+alt+m that would toggle mic using `amixer set Capture toggle`
+    xsetwacom --set $TABLET Button 10 "key +ctrl +alt +m -m -alt -ctrl"
+fi
